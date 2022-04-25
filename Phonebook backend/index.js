55f1519587a927dc263f1b6c63048b214e72ed88
Omar Baltaji <omar.baltaji@app4legal.com>
2022-04-25 15:22:56 +0300

    Phonebook backend step3

--- Phonebook backend/index.js
+++ Phonebook backend/index.js
@@ -34,6 +34,17 @@ app.get('/info', (req, res) => {
     res.send(`<div>${entries}</div><br><div>${date}</div>`);
 });
 
+app.get('/api/persons/:id', (req, res) => {
+    const id = Number(req.params.id);
+    const person = persons.find(person => person.id === id);
+
+    if(person) {
+        res.json(person);
+    } else {
+        res.status(404).end();
+    }
+})
+
 const PORT = 3001;
 app.listen(PORT, () => {
     console.log(`Server running from port ${PORT}`);
