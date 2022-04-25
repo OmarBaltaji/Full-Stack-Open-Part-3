e7ee494f5373d63fb728aef4a1e34cbc334f6a36
Omar Baltaji <omar.baltaji@app4legal.com>
2022-04-25 16:43:45 +0300

    Phonebook backend step8

--- Phonebook backend/index.js
+++ Phonebook backend/index.js
@@ -4,8 +4,11 @@ const morgan = require('morgan');
 
 app.use(express.json());
 
-app.use(morgan('tiny'));
+morgan.token('body', function getBody (req) {
+    return JSON.stringify(req.body);
+});
 
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'));
 
 let persons = [
     { 
