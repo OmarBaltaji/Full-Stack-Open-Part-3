67b88c3b40b1fac67975bdc82e5b4848f8c6bf26
Omar Baltaji <omar.baltaji@app4legal.com>
2022-04-30 21:35:54 +0300

    phonebook step9

--- Phonebook backend/index.js
+++ Phonebook backend/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const morgan = require('morgan');
+const cors = require('cors');
 
 app.use(express.json());
 
@@ -9,6 +10,7 @@ morgan.token('body', function getBody (req) {
 });
 
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'));
+app.use(cors());
 
 let persons = [
     { 
