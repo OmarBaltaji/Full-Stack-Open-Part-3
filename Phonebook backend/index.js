78a44fec3067233966bd3c9f8993aaec636c2a19
Omar Baltaji <omar.baltaji@app4legal.com>
2022-05-02 21:44:36 +0300

    Phonebook database, step1

--- Phonebook backend/index.js
+++ Phonebook backend/index.js
@@ -1,7 +1,9 @@
+require('dotenv').config();
 const express = require('express');
 const app = express();
 const morgan = require('morgan');
 const cors = require('cors');
+const Person = require('./models/person');
 
 app.use(express.json());
 
@@ -13,48 +15,26 @@ app.use(morgan(':method :url :status :res[content-length] - :response-time ms :b
 app.use(cors());
 app.use(express.static('build'));
 
-let persons = [
-    { 
-      "id": 1,
-      "name": "Arto Hellas", 
-      "number": "040-123456"
-    },
-    { 
-      "id": 2,
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
-    },
-    { 
-      "id": 3,
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
-    },
-    { 
-      "id": 4,
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
-    }
-];
-
 app.get('/api/persons', (req, res) => {
-    res.json(persons);
+    Person.find({}).then(persons => {
+        res.json(persons);
+    })
 });
 
 app.get('/info', (req, res) => {
-    const entries = `Phonebook has info for ${persons.length} people`;
-    const date = new Date();
-    res.send(`<div>${entries}</div><br><div>${date}</div>`);
+    Person.countDocuments({}).then(count => {
+        const entries = `Phonebook has info for ${count} people`;
+        const date = new Date();
+        res.send(`<div>${entries}</div><br><div>${date}</div>`);
+    })
 });
 
 app.get('/api/persons/:id', (req, res) => {
-    const id = Number(req.params.id);
-    const person = persons.find(person => person.id === id);
-
-    if(person) {
+    Person.findById(req.params.id).then(person => {
         res.json(person);
-    } else {
+    }).catch(error => {
         res.status(404).end();
-    }
+    })
 })
 
 app.delete('/api/persons/:id', (req, res) => {
@@ -91,7 +71,7 @@ app.post('/api/persons', (req, res) => {
     res.json(person);
 });
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
     console.log(`Server running from port ${PORT}`);
 });
